5dfa867080c9cdae2e1630a20bc40689f3cb87d5
Babe <115712127+Saadu10002@users.noreply.github.com>
2023-03-06 21:43:37 +0000

    update the update the changes made to the files

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# readme-generate
-![Github license](https://img.shield.io/badge/license-undefined-blue.svg)
+# Readme-generator
+![Github license](https://img.shields.io/badge/license-undefined-yellowgreen.svg)
+
+## Table of contents
+- [Description](#description)
+- [Instruction](#instruction)
+- [Installation](#insatalltion)
+- [Usage](#usesage)
+* [license] (#license)
+
+- [Credit](#credit)
+- [Contribution](#contribution)
+- [Question](#question)
+
+## Description
+This is a README file gerator, created from command line that uses the inquirer npm package to prompt questions to the users about their project information, using that information to generate a professional README file.
+
+## Instruction
+undefined
+
+## Installation
+undefined
+
+## Usage
+After installation of all the necessary tools, run node index.js from command line answer all the prompted questions, when you are done the README file will generated.
+
+## Credit
+undefined
+
+## Contribution
+undefined
+
+## Questions
+If got any questions, please reach out at GitHub: https://github.com/sfaaadu10003
+Email:NO@required
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,19 +3,56 @@
 
 function renderingLicenseBadge(license) {
   if(license !== "None")
-  return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
   return ``
   }
   
 
 // Function to return the license badge link
-function renderLicenseLink() {
- 
+// function renderLicenseLink() {
+  const renderLicenseLink = (license) => {
+  if(license !== `None`) {
+    return `* [license] (#license)\n`
+  }
+  return ``;
 }
 function generateMarkdown(data) {
-
   return `# ${data.title}
 ${renderingLicenseBadge(data.license)}
+
+## Table of contents
+- [Description](#description)
+- [Instruction](#instruction)
+- [Installation](#insatalltion)
+- [Usage](#usesage)
+${renderLicenseLink(data.license)}
+- [Credit](#credit)
+- [Contribution](#contribution)
+- [Question](#question)
+
+## Description
+${data.description}
+
+## Instruction
+Clone repository to your local machine, install inquirer, run node index.js
+ 
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credit
+${data.credit}
+
+## Contribution
+${data.contribution}
+
+## Questions
+If got any questions, please reach out at GitHub: https://github.com/${data.github}
+Email:${data.email}
+
 `;
 }
 
